e90c7a3204254ed174ac76ac11e73939840535c2
Gabriel Amaral <gabrielsared@gmail.com>
2025-12-10 20:30:38 -0300

    aula 03 - Comentários

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,17 @@
+""" Aula 03 - Comentários"""
+
+# Comentario de única linha
+
+'''
+Isso é um comentário 
+de múltiplas 
+linhas
+'''
+
+# atribui o valor 10 a variavel n1
+n1 = 10
+n2 = 30
+n3 = 78
+
+print(n1, n2)
+print(n3)
