ee8cec43efd82bc16df27a5fa4cc9dd52cebe458
Gabriel Amaral <gabrielsared@gmail.com>
2025-12-10 20:08:41 -0300

    aula 02 - Keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,23 @@
+""""Aula 02 Keywords e Identificadores"""
+
+nome = 'Maria'
+
+idade = 18
+MAIORIDADE = 18
+
+Nome = 'Joao'
+
+nome_completo = 'Maria Da Silva'
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+PI = 3.14
+
+if idade >= MAIORIDADE:
+    print('Maior de idade')
+else: 
+    print('Menor de idade')
