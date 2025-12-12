dea8caf03ef820b3dfeb8a56f6d7be64227b4f17
Gabriel Amaral <gabrielsared@gmail.com>
2025-12-12 17:11:55 -0300

    Execicio 01

---
+++ src/01-intro/02-controle-de-fluxo/02-Exercicios/ex01.py
@@ -0,0 +1,11 @@
+"""Exercicio 01"""
+
+# solicite oa usuario 3 notas e apresente o resultado da media aritmedica
+
+print('Escreva 3 notas para calcular a media')
+nota_1 = float(input('Escreva a primeira nota: '))
+nota_2 = float(input('Escreva a segunda nota: '))
+nota_3 = float(input('Escreva a terceira nota: '))
+
+media = (nota_1 + nota_2 + nota_3) / 3
+print('A media das provas é : ', media)
