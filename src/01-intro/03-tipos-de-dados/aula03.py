39951d9e59c41114ee54a540a092e4d638cced6e
Gabriel Amaral <gabrielsared@gmail.com>
2025-12-12 21:36:35 -0300

    Aula 03 - Set

---
+++ src/01-intro/03-tipos-de-dados/aula03.py
@@ -0,0 +1,27 @@
+"""Auls 03 - Set"""
+
+numeros = {1, 12, 5, 7, 4, 3, 3, 1}
+print(numeros, type(numeros))
+
+for numero in numeros:
+    print(numero)
+
+print(3 in numeros)
+print(50 in numeros)
+
+print(numeros)
+numeros.add(8)
+print(numeros)
+
+numeros2 = {1, 5, 4, 4, 3, 9}
+numeros.update(numeros2)
+print('-----')
+print(numeros)
+numeros2 = {1, 5, 4, 4, 3, 11, 9}
+print(numeros2)
+numeros.update(numeros2)
+print(numeros, type(numeros))
+
+
+
+
