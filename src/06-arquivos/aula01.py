446625581d7ec896d7b6b3a9727b87b5f4d6456b
Gabriel Amaral <gabrielsared@gmail.com>
2025-12-14 23:52:29 -0300

    Aula 01 - Manipulando Arquivos

---
+++ src/06-arquivos/aula01.py
@@ -0,0 +1,41 @@
+"""Aula 1 - Manipulando Arquivos"""
+
+# arquivo = open("src/06-arquivos/test.txt", "a")
+
+# print(arquivo.readable())
+#print(arquivo.read())
+
+#print(arquivo.readline())
+#print(arquivo.readline())
+#print(arquivo.readline())
+#print(arquivo.readline())
+
+# lista = arquivo.readlines()
+
+# print(lista)
+
+# print(lista[3])
+
+
+
+# arquivo.write("C\n")
+# arquivo.write("C++\n")
+# arquivo.write("Terraform\n")
+
+# arquivo.close()
+
+import os
+
+#os.remove()
+
+# if os.path.exists():
+#     os.remove()
+# else:
+#     print("arquivo nao existe")
+
+#os.rmdir()
+
+
+
+
+
